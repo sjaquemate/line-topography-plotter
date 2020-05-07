29ab3744eb579daeea7799c002a120baace0b34b
sjaquemate <52576833+sjaquemate@users.noreply.github.com>
2020-05-07 16:05:20 +0200

    Update line_plotting.py

--- line_plotting.py
+++ line_plotting.py
@@ -6,10 +6,6 @@ def get_segments(x, y, z):
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
     return segments
 
-def plot_segment_3d(ax, segment, segment_width, segment_color, alpha=1):
-    l_i, = ax.plot(segment[:, 0], segment[:, 1], segment[:, 2], linewidth=segment_width, c='black', alpha=alpha)
-    l_i.set_solid_capstyle('round')
-
 def plot_segment_2d(ax, segment, segment_width, segment_color, z_fraction, alpha=1):
     l_i, = ax.plot(segment[:, 0], segment[:, 1]+z_fraction*segment[:, 2], linewidth=segment_width, c=segment_color, alpha=alpha)
     l_i.set_solid_capstyle('round')
@@ -18,8 +14,3 @@ def plot_line_2d(ax, x, y, z, linewidths, linecolors, z_fraction):
     segments = get_segments(x, y, z)
     for i in range(len(segments)):
         plot_segment_2d(ax, segments[i], linewidths[i], linecolors[i], z_fraction)
-
-def plot_line_3d(ax, x, y, z, linewidths, linecolors):
-    segments = get_segments(x, y, z)
-    for i in range(len(segments)):
-        plot_segment_3d(ax, segments[i], linewidths[i], linecolors[i])
